b3b15d4ead074247fe0c82e9fd13d7cd74e7a7de
TogrulEminov <togrul40@mail.ru>
2024-05-14 17:14:39 +0400

    add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     breakpoints: {
       0: { slidesPerView: 1 },
-      450: { slidesPerView: 1.5},
+      450: { slidesPerView: 1.5 },
       768: { slidesPerView: 2 },
       1024: { slidesPerView: 3 },
     },
@@ -23,4 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     myBlogNavigation.classList.add("notNavigation");
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
 });
